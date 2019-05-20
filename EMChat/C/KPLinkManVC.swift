3a292b87682ed7c25ba2c2b0956e4ebbdeebb0ea
pxh <peixinhua@unisound.com>
2019-05-20 09:55:53 +0800

    变更项目结构

---
+++ EMChat/C/KPLinkManVC.swift
@@ -0,0 +1,19 @@
+//
+//  KPLinkManVC.swift
+//  EMChat
+//
+//  Created by pxh on 2019/5/20.
+//  Copyright © 2019 pxh. All rights reserved.
+//
+
+import UIKit
+
+/// 联系人界面
+class KPLinkManVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
